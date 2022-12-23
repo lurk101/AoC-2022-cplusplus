df2ee6e768e5f3c73f1c419771cb4a33cf358d5b
lurk101 <lurkone0one@gmail.com>
2022-12-23 14:15:05 -0500

    optimize day23

--- days/day23/day23.cpp
+++ days/day23/day23.cpp
@@ -3,9 +3,9 @@
 #include <climits>
 #include <fstream>
 #include <iostream>
-#include <map>
-#include <set>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 using namespace chrono;
@@ -17,83 +17,81 @@ const int EAST = 3;
 
 struct point {
     int x, y;
-    bool operator!=(const point& r) const { return x != r.x || y != r.y; }
-    bool operator<(const point& r) const {
-        if (y == r.y)
-            return x < r.x;
-        return y < r.y;
-    }
+    bool operator==(const point& r) const { return (x == r.x) && (y == r.y); }
+    point operator+(const point& r) const { return {x + r.x, y + r.y}; }
 };
 
-set<point> elves;
+struct hash_fn {
+    size_t operator()(const point& p) const { return p.x * p.y; }
+};
 
-point propose(int dir_ix, const point& elf) {
+const point N({0, -1}), S({0, 1}), E({1, 0}), W({-1, 0}), NE(N + E), NW(N + W), SE(S + E),
+    SW(S + W);
+
+unordered_set<point, hash_fn> elves;
+int dir_ix;
+
+point propose(const point& elf) {
     bool should_move = false;
-    for (int dx = -1; dx <= 1; dx++)
+    for (int dx = -1; dx <= 1; dx++) {
         for (int dy = -1; dy <= 1; dy++) {
-            if (dx == dy && dy == 0)
+            if ((dx == dy) && (dy == 0))
                 continue;
-            if (elves.count({elf.x + dx, elf.y + dy})) {
+            if (elves.count(elf + point({dx, dy}))) {
                 should_move = true;
                 break;
             }
         }
+    }
     if (!should_move)
-        return point({elf.x, elf.y});
+        return elf;
     for (int i = 0; i < 4; i++) {
         switch ((dir_ix + i) % 4) {
         case NORTH:
-            if (!elves.count({elf.x + 1, elf.y - 1}) && !elves.count({elf.x - 1, elf.y - 1}) &&
-                !elves.count({elf.x, elf.y - 1})) {
-                return point({elf.x, elf.y - 1});
-            } else
-                break;
+            if (!elves.count(elf + N) && !elves.count(elf + NE) && !elves.count(elf + NW))
+                return point(elf + N);
+            break;
         case SOUTH:
-            if (!elves.count({elf.x + 1, elf.y + 1}) && !elves.count({elf.x - 1, elf.y + 1}) &&
-                !elves.count({elf.x, elf.y + 1})) {
-                return point({elf.x, elf.y + 1});
-            } else
-                break;
+            if (!elves.count(elf + S) && !elves.count(elf + SE) && !elves.count(elf + SW))
+                return point(elf + S);
+            break;
         case WEST:
-            if (!elves.count({elf.x - 1, elf.y - 1}) && !elves.count({elf.x - 1, elf.y + 1}) &&
-                !elves.count({elf.x - 1, elf.y})) {
-                return point({elf.x - 1, elf.y});
-            } else
-                break;
+            if (!elves.count(elf + W) && !elves.count(elf + NW) && !elves.count(elf + SW))
+                return point(elf + W);
+            break;
         case EAST:
-            if (!elves.count({elf.x + 1, elf.y - 1}) && !elves.count({elf.x + 1, elf.y + 1}) &&
-                !elves.count({elf.x + 1, elf.y})) {
-                return point({elf.x + 1, elf.y});
-            } else
-                break;
+            if (!elves.count(elf + E) && !elves.count(elf + NE) && !elves.count(elf + SE))
+                return point(elf + E);
+            break;
         }
     }
-    return {elf.x, elf.y};
+    return elf;
 };
 
-bool mov(int dir_ix) {
-    map<point, point> proposed;
-    map<point, int> to_counts;
+bool mov() {
+    unordered_map<point, point, hash_fn> proposed;
+    unordered_map<point, int, hash_fn> to_counts;
     for (auto& elf : elves) {
-        auto proposed_move = propose(dir_ix, elf);
+        auto proposed_move = propose(elf);
         proposed[elf] = proposed_move;
         if (to_counts.count(proposed_move))
             to_counts[proposed_move]++;
         else
             to_counts[proposed_move] = 1;
     }
-    set<point> new_elves;
+    unordered_set<point, hash_fn> new_elves;
     bool elves_moved = false;
     for (auto& e : elves) {
         auto& proposed_move = proposed[e];
         if (to_counts[proposed_move] == 1) {
             new_elves.insert(proposed_move);
-            if (proposed_move != e)
+            if (!(proposed_move == e))
                 elves_moved = true;
         } else
             new_elves.insert(e);
     }
     elves = new_elves;
+    dir_ix = (dir_ix + 1) % 4;
     return elves_moved;
 }
 
@@ -111,11 +109,9 @@ int main() {
         }
         y++;
     }
-    int dir_ix = 0;
-    for (int i = 0; i < 10; i++) {
-        mov(dir_ix);
-        dir_ix = (dir_ix + 1) % 4;
-    }
+    int i;
+    for (i = 1; i <= 10; i++)
+        mov();
     int min_x = INT_MAX, max_x = INT_MIN, min_y = INT_MAX, max_y = INT_MIN;
     for (auto& e : elves) {
         if (e.x < min_x)
@@ -127,14 +123,10 @@ int main() {
         if (e.y > max_y)
             max_y = e.y;
     }
-    int part1 = (max_y - min_y + 1) * (max_x - min_x + 1) - int(elves.size());
-    int i;
-    for (i = 11;; i++) {
-        if (!mov(dir_ix))
+    int part1((max_y - min_y + 1) * (max_x - min_x + 1) - int(elves.size()));
+    for (;; i++)
+        if (!mov())
             break;
-        dir_ix = (dir_ix + 1) % 4;
-    }
-    cout << "Part 2: " << i + 1 << endl;
     cout << title << endl
          << "Part 1  - " << part1 << endl
          << "Part 2  - " << i << endl
